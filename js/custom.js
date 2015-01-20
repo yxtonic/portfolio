bc2d3d19c46e662454081702fa8c6737eb10f4c9
azywong <azywong@gmail.com>
2015-01-20 12:34:36 -0800

    adding contact form

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,58 @@
  * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
  * File Description: Place here your custom scripts
  */
+$(function() {
+
+    $("#footer-form").submit(function(event) {
+        event.preventDefault(); // prevent default submit behaviour
+        // get values from FORM
+        var name = $("#footer-form input#name").val();
+        var email = $("#footer-form input#email").val();
+        var message = $("#footer-form textarea#message").val();
+        var firstName = name; // For Success/Failure Message
+        // Check for white space in name for Success/Fail message
+        if (firstName.indexOf(' ') >= 0) {
+            firstName = name.split(' ').slice(0, -1).join(' ');
+        }
+        $.ajax({
+            url: "./mail/contact_me.php",
+            type: "POST",
+            data: {
+                name: name,
+                email: email,
+                message: message
+              }
+            }).success(
+                function() {
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                    console.log('success');
+                }).error(
+                function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                    console.log('failure');
+                });
+    });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
